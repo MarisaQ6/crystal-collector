5066127aeaffc4b589005d27ceaa0f68ab48fee8
Marisa Quijano <miss.quijano@gmail.com>
2019-06-05 21:22:00 -0400

    working on Js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,28 @@
 // game.js
 
+$(document).ready(function() {
+
+// set up math
+
+    var random = Math.floor(Math.random()*102+19);
+    $("#numberMatch").text(Random);
+
+// var arrays for crystals  
+
+    var blue= Math.floor(Math.random()*21+1);
+    var green= Math.floor(Math.random()*12+1);
+    var pink= Math.floor(Math.random()*12+1);
+    var purple= Math.floor(Math.random()*21+1);
+
+    
+
+
+
+
+
+
+
+
+
+
+}
